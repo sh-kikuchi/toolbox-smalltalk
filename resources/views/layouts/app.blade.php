5e758eb170ff6f6080242efb563cf25be9744db0
sh-kikuchi <shu94hist726@gmail.com>
2021-12-14 23:18:55 +0900

    erase_register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,11 +47,11 @@
                             <!-- <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li> -->
-                            @if (Route::has('register'))
+                            <!-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link text-white" href="{{ route('register') }}">新規登録はこちら</a>
                                 </li>
-                            @endif
+                            @endif -->
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
